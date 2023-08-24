53d407a9862107fec5662cf94e655c43133e6f5c
Duy Hoang <hoangduy241199@gmail.com>
2023-08-24 09:52:03 +0700

    [UPDATE]

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const status = require('../src/health/routes');
 const users = require('../src/users/routes');
+const home = require('../src/home/routes');
 // const validateAuth = require('../middlewares/validateAuth');
 // const getData = require('../middlewares/getData');
 
 module.exports = (app) => {
   app.use('/api/v1/status', status);
   app.use('/api/v1/users', users);
+  app.use('/', home);
   // app.use('/users', validateAuth.checkIfAuthenticated, getData.getGeoip, users);
   app.use('*', (req, res) => {
     res.send('Not found!!!');
